32c7f1923d98e0ec748b7de6673c35814cf0e2b9
Marie Roald <roald.marie@gmail.com>
2020-12-27 12:16:54 +0100

    Updated webpage

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
-import flask
-import random
 import json
+import random
+
+import flask
+
+
+__author__ = "Marie Roald & Yngve Mardal Moe"
+
 
 app = flask.Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-with open("haikus.json") as f:
+with open("static/haikus.json") as f:
     haikus = json.load(f)
+
 
 @app.route("/")
 def haiku():
     haiku_info = random.choice(haikus)
-    haiku = haiku_info['haiku'].replace("\n", "<br />")
-    html = f"<a href={haiku_info['link']}>{haiku}</a>"
-    return html
+    haiku = haiku_info["haiku"].splitlines()
+
+    return flask.render_template("haiku.html", url=haiku_info["link"], haiku=haiku)
 
 
 if __name__ == "__main__":
